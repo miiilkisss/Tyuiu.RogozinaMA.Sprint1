2391aa84e7fbdcb85f19218ac879c3d744e2790a
mila.rogo@yandex.ru <mila.rogo@yandex.ru>
2025-11-09 20:34:57 +0500

    Task0

--- Tyuiu.RogozinaMA.Sprint1.Task0.V2.Test/DataServiceTest.cs
+++ Tyuiu.RogozinaMA.Sprint1.Task0.V2.Test/DataServiceTest.cs
@@ -13,7 +13,7 @@ namespace Tyuiu.RogozinaMA.Sprint1.Task0.V2.Test
         {
             DataService ds = new DataService();
             var res = ds.Calculate();
-            Assert.AreEqual(1, res);
+            Assert.AreEqual(1.0, res);
         }
     }
 }
