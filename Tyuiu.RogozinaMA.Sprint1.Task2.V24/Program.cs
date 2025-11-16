e5817145f36ef870ab48572026d41463daad0bc7
mila.rogo@yandex.ru <mila.rogo@yandex.ru>
2025-11-16 19:16:24 +0500

    Task2

--- Tyuiu.RogozinaMA.Sprint1.Task2.V24/Program.cs
+++ Tyuiu.RogozinaMA.Sprint1.Task2.V24/Program.cs
@@ -18,7 +18,7 @@ namespace Tyuiu.RogozinaMA.Sprint1.Task2.V24
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #1                                                               *");
-            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Тема: Арифметические операторы в C#                                        *");
             Console.WriteLine("* Задание #2                                                              *");
             Console.WriteLine("* Вариант #24                                                             *");
             Console.WriteLine("* Выполнила: Рогозина Милана Андреевна | ИСПб-25-1                        *");
