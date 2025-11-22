fc3e3f46afd06e2f8bbc6fbbffe31fd85b5da522
mila.rogo@yandex.ru <mila.rogo@yandex.ru>
2025-11-22 19:24:07 +0500

    Task3

---
+++ Tyuiu.RogozinaMA.Sprint1.Task3.V16.Test/DataServiceTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.RogozinaMA.Sprint1.Task3.V16.Lib;
+
+namespace Tyuiu.RogozinaMA.Sprint1.Task3.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            double x1 = 2;
+            double x2 = 3;
+            double wait = -5.000; // -(2 + 3) = -5
+            double res = ds.Calculate(x1, x2);
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCalculateWithNegativeRoots()
+        {
+            DataService ds = new DataService();
+            double x1 = -1;
+            double x2 = -4;
+            double wait = 5.000; // -(-1 + -4) = -(-5) = 5
+            double res = ds.Calculate(x1, x2);
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCalculateWithFractionalRoots()
+        {
+            DataService ds = new DataService();
+            double x1 = 1.5;
+            double x2 = 2.5;
+            double wait = -4.000; // -(1.5 + 2.5) = -4
+            double res = ds.Calculate(x1, x2);
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCalculateWithZero()
+        {
+            DataService ds = new DataService();
+            double x1 = 0;
+            double x2 = 5;
+            double wait = -5.000; // -(0 + 5) = -5
+            double res = ds.Calculate(x1, x2);
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCalculateWithDecimalRounding()
+        {
+            DataService ds = new DataService();
+            double x1 = 1.234;
+            double x2 = 2.345;
+            double wait = -3.579; // -(1.234 + 2.345) = -3.579
+            double res = ds.Calculate(x1, x2);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.RogozinaMA.Sprint1.Task3.V16/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.RogozinaMA.Sprint1.Task3.V16.Lib;
+
+namespace Tyuiu.RogozinaMA.Sprint1.Task3.V16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Рогозина М.А. | ИСПб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнила: Рогозина Милана Андреевна | ИСПб-25-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("* Программа вычисляет коэффициент приведенного квадратного уравнения,     *");
+            Console.WriteLine("* корнями которого являются введенные два вещественных числа.             *");
+            Console.WriteLine("* (b = -x1 - x2). Ответ округлить до 3 знаков после запятой.              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            // Запрос данных у пользователя
+            Console.Write("Введите первый корень (x1): ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите второй корень (x2): ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            // Вычисление результата через библиотеку
+            double result = ds.Calculate(x1, x2);
+            Console.WriteLine($"Коэффициент b квадратного уравнения с корнями {x1} и {x2} = {result:F3}");
+
+            Console.ReadLine();
+        }
+    }
+}
